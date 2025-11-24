6b3f64d2d3dba9fccb7c6e4bad77132a7cba5eda
Artyom Opanchuk <subzero666@list.ru>
2025-11-24 23:14:11 +0300

    Спринт 7

---
+++ sprint7/main/a.py
@@ -0,0 +1,37 @@
+from __future__ import annotations
+
+import itertools
+import sys
+from collections.abc import Iterable
+
+
+def get_max_profit(prices: Iterable[int]) -> int:
+    prices_iter = iter(prices)
+
+    try:
+        previous_price = next(prices_iter)
+    except StopIteration:
+        return 0
+
+    result = 0
+
+    for price in prices:
+        if price > previous_price:
+            result += price - previous_price
+
+        previous_price = price
+
+    return result
+
+
+def main() -> None:
+    prices_count = int(input().strip())
+    prices = itertools.islice(
+        map(int, sys.stdin.readline().split()),
+        prices_count,
+    )
+    print(get_max_profit(prices))
+
+
+if __name__ == '__main__':
+    main()
---
+++ sprint7/main/b.py
@@ -0,0 +1,53 @@
+from __future__ import annotations
+
+from collections.abc import Iterable
+
+type Time = tuple[int, int]
+type Event = tuple[Time, Time]
+
+
+def generate_schedule(events: Iterable[Event]) -> Iterable[Event]:
+    events_list = list(events)
+    events_list.sort(key=lambda _event: (_event[1], _event[0]))
+    previous_end_time: Time = (0, 0)
+
+    for event in events_list:
+        start_time, end_time = event
+
+        if start_time >= previous_end_time:
+            yield event
+            previous_end_time = end_time
+
+
+def read_events(count: int) -> Iterable[Event]:
+    for i in range(count):
+        start_time, end_time = map(parse_time, input().split())
+        yield start_time, end_time
+
+
+def parse_time(time_str: str) -> Time:
+    values_list = list(map(int, time_str.strip().split('.')[:2]))
+
+    if len(values_list) < 2:
+        values_list.append(0)
+
+    return values_list[0], values_list[1]
+
+
+def render_time(time: Time) -> str:
+    return f'{time[0]}.{time[1]}' if time[1] else str(time[0])
+
+
+def main() -> None:
+    events_count = int(input().strip())
+    events = read_events(events_count)
+
+    schedule = list(generate_schedule(events))
+    print(len(schedule))
+
+    for start_time, end_time in schedule:
+        print(*(render_time(time) for time in (start_time, end_time)))
+
+
+if __name__ == '__main__':
+    main()
