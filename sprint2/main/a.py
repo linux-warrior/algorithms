b9f6b99249e0de0928ffe40da787e666ff8937ee
Artyom Opanchuk <subzero666@list.ru>
2026-01-20 23:38:29 +0300

    Рефакторинг

--- sprint2/main/a.py
+++ sprint2/main/a.py
@@ -3,6 +3,7 @@ from __future__ import annotations
 import itertools
 import sys
 from collections.abc import Iterable, Sequence
+from typing import Self
 
 
 class Matrix:
@@ -18,24 +19,27 @@ class Matrix:
         columns_count: int | None = None
 
         for elements_row in elements:
-            elements_list_row = list(elements_row)
+            elements_list_row: list[int]
 
             if columns_count is None:
+                elements_list_row = list(elements_row)
                 columns_count = len(elements_list_row)
+            else:
+                elements_list_row = list(itertools.islice(elements_row, columns_count))
 
-            elements_list.append(elements_list_row[:columns_count])
+            elements_list.append(elements_list_row)
             self.rows_count += 1
 
         self.columns_count = columns_count or 0
 
     @classmethod
-    def read(cls, rows_count: int, columns_count: int) -> Matrix:
+    def read(cls, rows_count: int, columns_count: int) -> Self:
         return cls(cls._read_elements(rows_count, columns_count))
 
     @classmethod
     def _read_elements(cls, rows_count: int, columns_count: int) -> Iterable[Iterable[int]]:
-        for row in range(rows_count):
-            elements_row = map(int, sys.stdin.readline().strip().split())
+        for row_num in range(rows_count):
+            elements_row = map(int, sys.stdin.readline().split())
             yield itertools.islice(elements_row, columns_count)
 
     def __str__(self) -> str:
@@ -46,17 +50,14 @@ class Matrix:
 
         return '\n'.join(result)
 
-    def transpose(self) -> Matrix:
-        elements_list: list[list[int]] = []
-
-        for i in range(self.columns_count):
-            elements_list.append([0] * self.rows_count)
+    def transpose(self) -> Self:
+        elements_list = [[0] * self.rows_count for _column_num in range(self.columns_count)]
 
         for row_num, elements_row in enumerate(self.elements):
             for column_num, element in enumerate(elements_row):
                 elements_list[column_num][row_num] = element
 
-        return Matrix(elements_list)
+        return self.__class__(elements_list)
 
 
 def read_int() -> int:
