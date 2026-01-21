3e0f7dd27c2b7784477be5006f60e7546247bf6c
Artyom Opanchuk <subzero666@list.ru>
2026-01-21 16:22:40 +0300

    Рефакторинг

--- sprint1/main/c.py
+++ sprint1/main/c.py
@@ -3,11 +3,11 @@ from __future__ import annotations
 import itertools
 import sys
 from collections.abc import Iterable, Sequence
+from typing import Self
 
 
 class Matrix:
     elements: Sequence[Sequence[int]]
-
     rows_count: int
     columns_count: int
 
@@ -16,25 +16,27 @@ class Matrix:
         self.elements = elements_list
 
         self.rows_count = 0
-        self.columns_count = 0
+        columns_count: int | None = None
 
         for elements_row in elements:
-            elements_list_row = list(elements_row)
+            elements_list_row: list[int]
+
+            if columns_count is None:
+                elements_list_row = list(elements_row)
+                columns_count = len(elements_list_row)
+            else:
+                elements_list_row = list(itertools.islice(elements_row, columns_count))
+
             elements_list.append(elements_list_row)
-
             self.rows_count += 1
-            columns_count = len(elements_list_row)
 
-            if columns_count > self.columns_count:
-                self.columns_count = columns_count
+        self.columns_count = columns_count or 0
 
     def is_valid_element(self, row: int, column: int) -> bool:
-        return all([
-            row >= 0,
-            row < self.rows_count,
-            column >= 0,
-            column < self.columns_count,
-        ])
+        return (
+                0 <= row < self.rows_count and
+                0 <= column < self.columns_count
+        )
 
     def get_neighbours(self, row: int, column: int) -> Sequence[int]:
         if not self.is_valid_element(row, column):
@@ -58,26 +60,30 @@ class Matrix:
 
         return result
 
+    @classmethod
+    def read(cls, rows_count: int, columns_count: int) -> Self:
+        return cls(cls._read_elements(rows_count, columns_count))
+
+    @classmethod
+    def _read_elements(cls, rows_count: int, columns_count: int) -> Iterable[Iterable[int]]:
+        for row_num in range(rows_count):
+            elements_row = map(int, sys.stdin.readline().split())
+            yield itertools.islice(elements_row, columns_count)
+
 
 def read_int() -> int:
     return int(input().strip())
 
 
-def read_int_table(rows_count: int, columns_count: int) -> Iterable[Iterable[int]]:
-    for row in range(rows_count):
-        elements_row = map(int, sys.stdin.readline().strip().split())
-        yield itertools.islice(elements_row, columns_count)
-
-
 def main() -> None:
     rows_count = read_int()
     columns_count = read_int()
-    matrix = Matrix(read_int_table(rows_count, columns_count))
-
+    matrix = Matrix.read(rows_count, columns_count)
     row = read_int()
     column = read_int()
+
     neighbours = matrix.get_neighbours(row, column)
-    print(' '.join(map(str, neighbours)))
+    print(*neighbours)
 
 
 if __name__ == '__main__':
