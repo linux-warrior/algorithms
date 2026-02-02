0c2db17fb80dce702ce9bee15fd57fc152b77283
Artyom Opanchuk <subzero666@list.ru>
2026-02-02 17:26:52 +0300

    Оптимизация алгоритма

--- sprint1/final/a.py
+++ sprint1/final/a.py
@@ -1,49 +1,59 @@
-# https://contest.yandex.ru/contest/22450/run-report/140272170/
+# https://contest.yandex.ru/contest/22450/run-report/156221955/
 
 from __future__ import annotations
 
 import itertools
 import sys
-from collections.abc import Sequence
+from collections.abc import Iterable
 
 
-def get_distances_to_zero(numbers: Sequence[int]) -> Sequence[int]:
-    result: list[int] = []
-    distance = len(numbers)
+def get_distances_to_zero(numbers: Iterable[str]) -> Iterable[int]:
+    zeros_positions: list[int] = []
+    i = -1
 
-    for number in numbers:
-        if number == 0:
-            distance = 0
+    for i, number_str in enumerate(numbers):
+        if number_str == '0':
+            zeros_positions.append(i)
+
+    zeros_count = len(zeros_positions)
+
+    if not zeros_count:
+        return
+
+    numbers_count = i + 1
+    zero_pos = zeros_positions[0]
+    next_zero = 1
+    previous_zero_pos: int | None = None
+
+    for i in range(numbers_count):
+        if i != zero_pos:
+            distance = abs(i - zero_pos)
+
+            if previous_zero_pos is not None:
+                distance = min(distance, i - previous_zero_pos)
+
+            yield distance
+            continue
+
+        yield 0
+
+        if next_zero < zeros_count:
+            previous_zero_pos = zero_pos
+            zero_pos = zeros_positions[next_zero]
+            next_zero += 1
         else:
-            distance += 1
-
-        result.append(distance)
-
-    distance = len(numbers)
-
-    for i in range(len(numbers) - 1, -1, -1):
-        number = numbers[i]
-
-        if number == 0:
-            distance = 0
-        else:
-            distance += 1
-
-        if distance < result[i]:
-            result[i] = distance
-
-    return result
+            previous_zero_pos = None
 
 
 def main() -> None:
     numbers_count = int(input().strip())
-    numbers = list(itertools.islice(
-        map(int, sys.stdin.readline().strip().split()),
+    numbers = itertools.islice(
+        sys.stdin.readline().split(),
         numbers_count,
-    ))
+    )
 
-    distances_list = get_distances_to_zero(numbers)
-    print(*distances_list)
+    distances_iter = get_distances_to_zero(numbers)
+    print(*distances_iter)
 
 
 if __name__ == '__main__':
